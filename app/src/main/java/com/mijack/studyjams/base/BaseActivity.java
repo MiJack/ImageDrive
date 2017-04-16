5d329a3a9ff000f9277e95adb2b3b747588da504
=Mi&Jack <=893380824@qq.com>
2017-04-16 22:41:43 +0800

    create project

---
+++ app/src/main/java/com/mijack/studyjams/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.mijack.studyjams.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Mr.Yuan
+ * @date 2017/4/16
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/mijack/studyjams/ui/MainActivity.java
@@ -0,0 +1,10 @@
+package com.mijack.studyjams.ui;
+
+import com.mijack.studyjams.base.BaseActivity;
+
+/**
+ * @author Mr.Yuan
+ * @date 2017/4/16
+ */
+public class MainActivity extends BaseActivity {
+}
