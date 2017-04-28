9622177dc9195ea67dd2e7a0c81e5b1f5ca3e6b3
=Mi&Jack <=893380824@qq.com>
2017-04-28 22:40:39 +0800

    完善首页的结构

---
+++ app/src/main/java/cn/studyjams/s220170131/mijack/ui/AboutActivity.java
@@ -0,0 +1,10 @@
+package cn.studyjams.s220170131.mijack.ui;
+
+import cn.studyjams.s220170131.mijack.base.BaseActivity;
+
+/**
+ * @author Mr.Yuan
+ * @date 2017/4/28
+ */
+public class AboutActivity extends BaseActivity {
+}
--- app/src/main/java/cn/studyjams/s220170131/mijack/ui/MainActivity.java
+++ app/src/main/java/cn/studyjams/s220170131/mijack/ui/MainActivity.java
@@ -1,30 +1,28 @@
 package cn.studyjams.s220170131.mijack.ui;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.List;
-
 import cn.studyjams.s220170131.mijack.R;
-import cn.studyjams.s220170131.mijack.adapter.ImageAdapter;
 import cn.studyjams.s220170131.mijack.base.BaseActivity;
 import cn.studyjams.s220170131.mijack.componment.NavigationHeaderView;
-import cn.studyjams.s220170131.mijack.core.MediaManager;
-import cn.studyjams.s220170131.mijack.entity.Image;
-import cn.studyjams.s220170131.mijack.entity.Media;
+import cn.studyjams.s220170131.mijack.fragment.BackUpFragment;
+import cn.studyjams.s220170131.mijack.fragment.ImageDriverFragment;
 import cn.studyjams.s220170131.mijack.fragment.ImageListFragment;
 
 /**
@@ -33,13 +31,20 @@ import cn.studyjams.s220170131.mijack.fragment.ImageListFragment;
  */
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final int REQUEST_CODE_LOGIN = 1;
+    private static final int IMAGE_LIST_FRAGMENT = 1;
+    private static final int IMAGE_DRIVER_FRAGMENT = 2;
+    private static final int BACKUP_FRAGMENT = 3;
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationHeaderView headerView;
     NavigationView navigationView;
     private FirebaseAuth firebaseAuth;
-    ImageListFragment fragment;
+    ImageListFragment imageListFragment;
+    ImageDriverFragment imageDriverFragment;
+    Fragment currentFragment = null;
+    private BackUpFragment backUpFragment;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,11 +61,45 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.content_drawer_open, R.string.content_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-        fragment = new ImageListFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.frameLayout, fragment)
-                .commit();
-        navigationView.setNavigationItemSelectedListener(this);
+        switchFragment(IMAGE_LIST_FRAGMENT);
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    private void switchFragment(int fragmentCode) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (currentFragment != null) {
+            transaction.hide(currentFragment);
+        }
+        switch (fragmentCode) {
+            case IMAGE_LIST_FRAGMENT:
+                if (imageListFragment == null) {
+                    imageListFragment = new ImageListFragment();
+                    transaction.add(R.id.frameLayout, imageListFragment);
+                } else {
+                    transaction.show(imageListFragment);
+                }
+                currentFragment = imageListFragment;
+                break;
+            case IMAGE_DRIVER_FRAGMENT:
+                if (imageDriverFragment == null) {
+                    imageDriverFragment = new ImageDriverFragment();
+                    transaction.add(R.id.frameLayout, imageDriverFragment);
+                } else {
+                    transaction.show(imageDriverFragment);
+                }
+                currentFragment = imageDriverFragment;
+                break;
+            case BACKUP_FRAGMENT:
+                if (backUpFragment == null) {
+                    backUpFragment = new BackUpFragment();
+                    transaction.add(R.id.frameLayout, backUpFragment);
+                } else {
+                    transaction.show(backUpFragment);
+                }
+                currentFragment = backUpFragment;
+                break;
+        }
+        transaction.commit();
     }
 
     @Override
@@ -73,10 +112,10 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.actionShowFolder:
-                fragment.showFolder();
+                imageListFragment.showFolder();
                 break;
             case R.id.actionShowImages:
-                fragment.showImages();
+                imageListFragment.showImages();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -85,23 +124,45 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.actionDriver:
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                switchFragment(IMAGE_DRIVER_FRAGMENT);
+                return true;
+            case R.id.actionLocal:
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                switchFragment(IMAGE_LIST_FRAGMENT);
+                return true;
+            case R.id.actionBackUp:
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                switchFragment(BACKUP_FRAGMENT);
+                return true;
+            case R.id.actionAbout:
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            case R.id.actionSettings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
             case R.id.actionLogout:
-                DialogInterface.OnClickListener listener =
-                        (DialogInterface dialog, int which) -> {
-                            if (which == DialogInterface.BUTTON_POSITIVE) {
-                                firebaseAuth.signOut();
-                                headerView.loadLoginInfo();
-                            }
-                        };
-                new AlertDialog.Builder(this)
-                        .setTitle("Sign out")
-                        .setIcon(R.drawable.ic_logout)
-                        .setCancelable(false)
-                        .setPositiveButton("确定", listener)
-                        .setNegativeButton("取消", listener)
-                        .setMessage("你确定要退出吗？")
-                        .create().show();
-                break;
+                if (dialog == null) {
+                    DialogInterface.OnClickListener listener =
+                            (DialogInterface dialog, int which) -> {
+                                if (which == DialogInterface.BUTTON_POSITIVE) {
+                                    firebaseAuth.signOut();
+                                    headerView.loadLoginInfo();
+                                }
+                            };
+                    dialog = new AlertDialog.Builder(this)
+                            .setTitle("Sign out")
+                            .setIcon(R.drawable.ic_logout)
+                            .setCancelable(false)
+                            .setPositiveButton("确定", listener)
+                            .setNegativeButton("取消", listener)
+                            .setMessage("你确定要退出吗？")
+                            .create();
+                }
+                dialog.show();
+                return true;
         }
         return false;
     }
